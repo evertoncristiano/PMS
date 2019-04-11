bdfec7c80f639df39a6cae1b4dd5b3fdd89f129d
Everton Cristiano dos Santos <>
2019-04-11 17:53:38 -0300

    Project view started

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -14,8 +14,56 @@
                 @method('DELETE')
                 <a href="\projects" class="btn btn-sm btn-outline-dark">Voltar</a>
                 <a href="\projects\{{$project->id}}\edit" class="btn btn-sm btn-dark">Editar</a>
-                <button type="submit" class="btn btn-sm btn-danger">Excluir</a>
+                <button type="submit" class="btn btn-sm btn-danger">Excluir</button>
             </form>
+
+            <div class="col-12 mt-5 mb-5">
+                <p>{{$project->description}}</p>
+            </div>
+
+            <div class="row">
+                <div class="col-md-7">
+                    <h2 class="h5">Tarefas</h2>
+                    <table class="table table-sm table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Usuário</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                <tr>
+                                    <td><a href="/projects">Criar tela de Cadastro de usuário</a></td>
+                                    <td>Everton</td>
+                                </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="col-md-5">
+                    <h2 class="h5">Usuários</h2>
+                    <table class="table table-sm table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="col-md-8">Usuário</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Everton</td>
+                                <td>
+                                    <form action="" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-danger">Remover</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
         </div>     
     </div>
