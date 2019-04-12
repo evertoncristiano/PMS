70c14b283730acbfe8dd9176006ac0cb5de5e41b
Everton Cristiano dos Santos <>
2019-04-11 21:22:47 -0300

    Fix project view, project cards layout

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -22,13 +22,15 @@
 
             <div class="row">
                 @foreach($projects as $project)
-                <div class="card mb-3 col-md-4">
-                    <div class="row no-gutters">
-                        <div class="card-body">
-                            <h5 class="card-title">{{$project->name}}</h5>
-                            <p class="card-text">{{ substr($project->description, 0, 140)}} ...</p>
-                            <p class="card-text"><small class="text-muted">Data de Entrega: {{$project->deliveryDate}}</small></p>
-                            <a href="/projects/{{$project->id}}" class="btn btn-dark col-12">Ver Projeto</a>
+                <div class="col-xl-4">
+                    <div class="card mb-3">
+                        <div class="row no-gutters">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$project->name}}</h5>
+                                <p class="card-text">{{ substr($project->description, 0, 140)}} ...</p>
+                                <p class="card-text"><small class="text-muted">Data de Entrega: {{$project->deliveryDate}}</small></p>
+                                <a href="/projects/{{$project->id}}" class="btn btn-dark col-12">Ver Projeto</a>
+                            </div>
                         </div>
                     </div>
                 </div>
